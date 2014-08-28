1120af4d4cad64685af98bf636da0486ebbdc6c3
Luiz Fernando Noschang <noschang@univali.br>
2014-08-28 17:40:03 -0300

    Melhorando instalador, incluindo jvm embutida, alterando imagens de local

---
+++ arquivos/linux/executar-studio-linux.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+caminho_script=$(readlink -f "$0")
+caminho_portugol=$(dirname "$caminho_script")
+caminho_java="$caminho_portugol/java/bin/java"
+
+numero_parametros=$#
+
+
+if [ $numero_parametros -gt 0 ]; then
+
+	lista_argumentos=""
+
+	for argumento; do	
+
+		if [ -f "$argumento" ]; then
+
+			argumento=$(readlink -f "$argumento")
+	
+			argumento="'$argumento'"
+		fi
+
+		lista_argumentos="$lista_argumentos $argumento"
+		
+	done
+
+	clr
+	
+	comando="'$caminho_java' -jar inicializador-ps.jar $lista_argumentos"
+
+	cd "$caminho_portugol"
+
+	sh -c "$comando"
+else
+
+	cd "$caminho_portugol"
+
+	"$caminho_java" -jar inicializador-ps.jar "$@"
+
+fi
