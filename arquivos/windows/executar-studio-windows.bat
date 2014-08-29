da11eb764caae8417aa24136fd61cafe09a5c8b1
Luiz Fernando Noschang <noschang@univali.br>
2014-08-29 14:33:50 -0300

    Implementando scripts de execucao multi-plataforma para Windows

--- arquivos/windows/executar-studio-windows.bat
+++ arquivos/windows/executar-studio-windows.bat
@@ -1,1 +1,43 @@
-REM Implementar
+@ECHO OFF
+
+SETLOCAL ENABLEDELAYEDEXPANSION
+
+CLS
+
+SET caminho_script=%~dp0
+SET caminho_script=%caminho_script:~0,-1%
+SET caminho_portugol=%caminho_script%
+SET caminho_java=%caminho_portugol%\java\java-windows\bin\javaw.exe
+
+SET comando="%caminho_java%" -jar inicializador-ps.jar
+SET parametros=
+
+:PROCESSAR_PROXIMO_PARAMETRO
+IF NOT "%~1"=="" (	
+
+	PUSHD .
+	CD /d "%caminho_script%"
+	SET parametro=%~f1
+	POPD	
+		
+	IF EXIST "!parametro!" (
+			
+		SET parametros=%parametros% "!parametro!"
+			
+	) ELSE (
+
+		SET parametros=%parametros% %1
+	)
+	
+	SHIFT
+	GOTO PROCESSAR_PROXIMO_PARAMETRO
+)
+
+SET comando=%comando%%parametros% 
+
+PUSHD .
+CD /d "%caminho_portugol%"
+%comando%
+POPD
+
+ENDLOCAL
